54e7464aac9cef346c9e1312fc9a9b43eb9cf2e3
Markus Appel <markospoparkos@gmail.com>
2025-02-25 18:27:32 +0100

    remove asset inline limit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    // phaser doesn't accept inlined assets
-    assetsInlineLimit: 0
   }
 })
